6da02b7999c640e185769157de04afb9e019ed5a
Padmini0 <psinghtanwar07@gmail.com>
2026-05-18 23:15:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin",  value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "POST, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
